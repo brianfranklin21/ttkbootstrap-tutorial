f64e453b576c91d7c30a835998eb2e453c4dd54d
Brian <33679241+brianfranklin21@users.noreply.github.com>
2024-03-21 23:02:16 -0700

    Initial commit

---
+++ tutorial.py
@@ -0,0 +1,31 @@
+"""Module Docstring"""
+
+import ttkbootstrap as ttk
+
+root = ttk.Window(themename="superhero")
+
+b1 = ttk.Button(root, text="primary", bootstyle="primary")
+b1.pack(side="left", padx=5, pady=5)
+
+b2 = ttk.Button(root, text="secondary", bootstyle="secondary")
+b2.pack(side="left", padx=5, pady=5)
+
+b3 = ttk.Button(root, text="success", bootstyle="success")
+b3.pack(side="left", padx=5, pady=5)
+
+b4 = ttk.Button(root, text="info", bootstyle="info")
+b4.pack(side="left", padx=5, pady=5)
+
+b5 = ttk.Button(root, text="warning", bootstyle="warning")
+b5.pack(side="left", padx=5, pady=5)
+
+b6 = ttk.Button(root, text="danger", bootstyle="danger")
+b6.pack(side="left", padx=5, pady=5)
+
+b7 = ttk.Button(root, text="light", bootstyle="light")
+b7.pack(side="left", padx=5, pady=5)
+
+b8 = ttk.Button(root, text="dark", bootstyle="dark")
+b8.pack(side="left", padx=5, pady=5)
+
+root.mainloop()
